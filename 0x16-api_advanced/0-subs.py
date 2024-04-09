4306392472e82671449bf3c28e381796224d8208
A-Yehia19 <abdullah.yehia19@gmail.com>
2024-04-09 16:41:30 +0200

    restrict redirections

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     url = "https://www.reddit.com/r/{}/about/.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     try:
         return response.json().get("data").get("subscribers")
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,7 +9,7 @@ def top_ten(subreddit):
     url = "https://www.reddit.com/r/{}/hot/.json?limit=10".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
 
-    response = requests.get(url, headers=headers)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     try:
         for post in response.get('data').get('children'):
