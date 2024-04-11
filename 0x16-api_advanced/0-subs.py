15e09d38759176ecc87818707069e76afaf45a28
A-Yehia19 <abdullah.yehia19@gmail.com>
2024-04-11 19:23:22 +0200

    test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 
 def number_of_subscribers(subreddit):
     '''get number of subscribers'''
-    url = "https://www.reddit.com/r/{}/about/.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
@@ -15,12 +15,3 @@ def number_of_subscribers(subreddit):
         return response.json().get("data").get("subscribers")
     else:
         return 0
-
-import sys
-
-if __name__ == '__main__':
-    number_of_subscribers = __import__('0-subs').number_of_subscribers
-    if len(sys.argv) < 2:
-        print("Please pass an argument for the subreddit to search.")
-    else:
-        print("{:d}".format(number_of_subscribers(sys.argv[1])))
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -6,13 +6,13 @@ import requests
 
 def top_ten(subreddit):
     '''get top ten posts from subreddit'''
-    url = "https://www.reddit.com/r/{}/hot/.json?limit=10".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
 
-    if response.status_code != 200:
-        for post in response.get('data').get('children'):
+    if response.status_code == 200:
+        for post in response.json().get('data').get('children'):
             print(post.get('data').get('title'))
     else:
         print(None)
